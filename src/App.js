146cdece47663e60f16a4561af67d2f323ce014c
FLopez73 <flopez730@gmail.com>
2023-05-05 12:04:29 -0700

    working on test still

--- src/App.js
+++ src/App.js
@@ -16,7 +16,12 @@ function App() {
 
   const createCat = (createdCat) => {
     console.log(createdCat)
-  };
+  }
+
+  const updateCat = (cat, id) => {
+    console.log("cat:", cat)
+    console.log("id:", id)
+  }
 
   return (
     <>
@@ -26,7 +31,10 @@ function App() {
         <Route path="/catindex" element={<CatIndex cats={cats} />} />
         <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
         <Route path="/catnew" element={<CatNew createCat={createCat} />} />
-        <Route path="/catedit" element={<CatEdit />} />
+        <Route
+          path="/catedit/:id"
+          element={<CatEdit cats={cats} updateCat={updateCat} />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
