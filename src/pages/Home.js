dc39f55907fa00c6584c8eb2a8b2f8b2b1db2b59
wagnergon <wagnergon52@gmail.com>
2023-05-08 10:09:30 -0700

    navlink fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "reactstrap";
-import { Link, NavLink } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 import "./home.css";
 
 const Home = () => {
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Navlink } from "react-router-dom";
 import { Container, Button } from "reactstrap";
 import "./notfound.css";
 
